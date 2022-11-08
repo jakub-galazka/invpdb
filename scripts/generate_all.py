0e0409e5f8434eff8ba84095ba01e43605505508
galik93 <galik93@gmail.com>
2022-11-08 21:45:22 +0100

    generate software

--- scripts/generate_all.py
+++ scripts/generate_all.py
@@ -7,6 +7,7 @@ from generate_room import Room
 from generate_cpu import generate_cpu
 from generate_gpu import generate_gpu
 from generate_ram_stick import generate_ram_stick
+from generate_software import Software
 from const import CSV_PATH
 
 
@@ -19,6 +20,7 @@ QUANTITY_BUILDINGS = 10
 QUANTITY_ROOMS = 1000
 QUANTITY_CPU_GPU = 100_000
 QUANTITY_RAM_STICK = 200_000
+QUANTITY_SOFTWARE = 200_000
 
 def main():
     if not os.path.exists(CSV_PATH % "account"): Accounts(QUANTITY_ACCOUNTS)
@@ -27,6 +29,7 @@ def main():
     if not os.path.exists(CSV_PATH % "cpu"): generate_cpu(QUANTITY_CPU_GPU)
     if not os.path.exists(CSV_PATH % "gpu"): generate_gpu(QUANTITY_CPU_GPU)
     if not os.path.exists(CSV_PATH % "ram_stick"): generate_ram_stick(QUANTITY_RAM_STICK)
+    if not os.path.exists(CSV_PATH % "software"): Software(QUANTITY_SOFTWARE)
 
 if __name__ == "__main__":
     main()
