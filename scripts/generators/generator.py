6fa9c6e146e16e14e972f58ca36b2a3c4d5d8138
MarceliG <marcel9817@gmail.com>
2022-11-17 21:44:48 +0100

    format_code

--- scripts/generators/generator.py
+++ scripts/generators/generator.py
@@ -4,13 +4,14 @@ import pandas as pd
 from .csv_writer import CSVWriter
 from abc import ABC, abstractmethod
 
+
 class Generator(ABC):
 
-    NULL_RATE = .25
+    NULL_RATE = 0.25
 
     def __init__(self, seed: int = None) -> None:
         self.csvw = CSVWriter()
-        if not seed == None:
+        if seed is not None:
             random.seed(seed)
 
     @abstractmethod
@@ -18,13 +19,24 @@ class Generator(ABC):
         pass
 
     def generate_list(self, quantity: int, fun, null_rate: int = None) -> list:
-        if not null_rate == None:
-            return ["" if random.random() < null_rate else fun() for _ in range(quantity)]
+        if null_rate is not None:
+            return [
+                "" if random.random() < null_rate else fun()
+                for _ in range(quantity)
+            ]
         return [fun() for _ in range(quantity)]
 
-    def sample_df(self, df: pd.DataFrame, column_name: str, index: np.ndarray) -> list:
+    def sample_df(
+        self, df: pd.DataFrame, column_name: str, index: np.ndarray
+    ) -> list:
         return df.iloc[index][column_name].tolist()
 
     def get_ids(self, csv_name: str, quantity: int) -> list[int]:
-        ids = pd.read_csv(self.csvw.csv_path % csv_name, usecols=[csv_name + "_id"]).squeeze().tolist()
+        ids = (
+            pd.read_csv(
+                self.csvw.csv_path % csv_name, usecols=[csv_name + "_id"]
+            )
+            .squeeze()
+            .tolist()
+        )
         return random.choices(ids, k=quantity)
